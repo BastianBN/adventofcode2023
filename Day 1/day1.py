c2576dc3564a610e728dddcb22aa7a37fd67e712
Banzai <baba83.bn@gmail.com>
2023-12-01 23:47:55 +0100

    TODO Day one part two

--- Day 1/day1.py
+++ Day 1/day1.py
@@ -1,15 +1,22 @@
-digits = []
-results = []
-f = open('data.txt', 'r')
-for line in f.readlines():
-    numbers = []
-    for c in line:
-        if c.isnumeric() :
-            numbers.append(c)
-    digits.append(numbers)
-for line in digits:
-    results.append(int(f"{line[0]}{line[-1]}"))
+def part1():
+    digits = []
+    results = []
+    f = open('data.txt', 'r')
+    for line in f.readlines():
+        numbers = []
+        for c in line:
+            if c.isnumeric() :
+                numbers.append(c)
+        digits.append(numbers)
+    for line in digits:
+        results.append(int(f"{line[0]}{line[-1]}"))
 
-print(sum(results))
+    print(sum(results))
+
+def part2():
+    pass
+
+if __name__ == "main":
+    part1()
 
 # or ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
