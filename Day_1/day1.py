e2713fa0812f5cf457cfec38bb260bbc14edb7b9
Banzai <baba83.bn@gmail.com>
2023-12-02 01:32:47 +0100

    reformate name of folder

--- Day_1/day1.py
+++ Day_1/day1.py
@@ -15,7 +15,6 @@ def part1():
     print(sum(results))
 
 def part2():
-    results = []
     cal_values = []
     nbs = {"zero":0, "one":1, "two":2, "three":3, "four":4, "five":5, "six":6, "seven":7, "eight":8, "nine":9}
     f = open('data.txt', 'r')
